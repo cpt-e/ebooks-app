f5b9ac23c7dcfbc7f3f4fec41eb044e7a8061771
24blank <tamamocross@protonmail.ch>
2022-04-11 13:03:42 +0400

    Config change

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBwny-93ZP1ayO3nwZbL3DvUy1tLGeiUyY",
-    authDomain: "ebooks-0404.firebaseapp.com",
-    projectId: "ebooks-0404",
-    storageBucket: "ebooks-0404.appspot.com",
-    messagingSenderId: "255299854761",
-    appId: "1:255299854761:web:490e66374524fa325fa4b8"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
